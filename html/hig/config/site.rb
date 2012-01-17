ccaf9e0b8d917be14f9b0f27437d565ed413a235
Jakub Steiner <jimmac@gmail.com>
2012-01-17 18:18:32 +0100

    / ‘html/hig/config/site.rb’

--- html/hig/config/site.rb
+++ html/hig/config/site.rb
@@ -2,8 +2,8 @@
 # configuration.preview_server_port = 3000
  
 # Default is localhost
-configuration.preview_server_host = "kratos"
-#configuration.preview_server_host = "localhost"
+#configuration.preview_server_host = "kratos"
+configuration.preview_server_host = "localhost"
  
 # Default is true
 # When false .html & index.html get stripped off generated urls
