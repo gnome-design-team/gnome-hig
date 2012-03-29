2b55b078be85b24e7eddbe86570f6e1575193680
Jakub Steiner <jimmac@gmail.com>
2012-03-29 20:43:45 +0200

    / ‘cheatsheet/render-localized-pdf.sh’

--- cheatsheet/render-localized-pdf.sh
+++ cheatsheet/render-localized-pdf.sh
@@ -2,10 +2,10 @@ for file in po/*po;
 do
 	lang=`echo $file | sed 's/po\/\(.*\)\.po/\1/gi'`
 	mkdir -p ./out/$lang
-	xml2po -a -p $file gettingstarted.svg > out/$lang/gettingstarted.svg
-	xml2po -a -p $file cheatsheet-01.svg > out/$lang/cheatsheet-01.svg
-	xml2po -a -p $file cheatsheet-02.svg > out/$lang/cheatsheet-02.svg
-	xml2po -a -p $file cheatsheet-03.svg > out/$lang/cheatsheet-03.svg
+	xml2po -a -p $file src/gettingstarted.svg > out/$lang/gettingstarted.svg
+	xml2po -a -p $file src/cheatsheet-01.svg > out/$lang/cheatsheet-01.svg
+	xml2po -a -p $file src/cheatsheet-02.svg > out/$lang/cheatsheet-02.svg
+	xml2po -a -p $file src/cheatsheet-03.svg > out/$lang/cheatsheet-03.svg
 	#create eps
 	#inkscape -z -A out/$lang/gettingstarted.pdf out/$lang/gettingstarted.svg
 	#remove svgs
